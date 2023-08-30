e78b1f342564b382725659a954a3c8707e6e001e
scscp <143437421+scscp@users.noreply.github.com>
2023-08-31 02:17:54 +0700

    Update xolpanel.sh

--- xolpanel.sh
+++ xolpanel.sh
@@ -3,8 +3,8 @@
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/kenDevXD/scupdate.git
-unzip scupdate/xolpanel.zip
+git clone https://github.com/scscp/scupdate.git
+unzip scupdate/ftvpn.zip
 pip3 install -r xolpanel/requirements.txt
 pip3 install pillow
 
